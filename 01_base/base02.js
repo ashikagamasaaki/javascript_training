59b99effc77ae21de0766412b5ce50f5cda591d4
ashikagamasaaki <gamasaga0312@gmail.com>
2024-02-14 23:21:01 +0900

    javascript学習開始

---
+++ 01_base/base02.js
@@ -0,0 +1,17 @@
+// 上書きOK
+var val1 = "var変数"
+console.log(val1)
+
+val1 = "上書き"
+console.log(val1)
+
+var val1 = "再宣言"
+console.log(val1)
+
+
+let val2 = "let変数"
+console.log(val2)
+
+val2 = "let上書き"
+console.log(val2)
+
